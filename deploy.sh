cd16fe639468d23cc11c8ff3d080f833b760cf4b
starinje <jared.starin@alphapoint.com>
2018-10-16 15:34:06 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+echo "hello world"
+# docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+# docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+# docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
+# docker push stephengrider/multi-client:latest
 
-docker push devopdockerregistry/ubiq-client:test
-docker push devopdockerregistry/ubiq-node:test
+# docker push devopdockerregistry/ubiq-client:test
+# docker push devopdockerregistry/ubiq-node:test
 
-kubectl apply -f k8s
+# kubectl apply -f k8s
